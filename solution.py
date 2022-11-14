48a071d3c0cc7ed619819763f9165981b1964615
kevinqmcdonald <kevinqmcdonald@gmail.com>
2022-11-14 14:21:26 -0500

    solution

--- solution.py
+++ solution.py
@@ -1,3 +1,37 @@
-# Solution code goes here
+import math
+from collections import defaultdict
+
+
 def valid_solution(board):
-    pass
+    rows = defaultdict(list)
+    cols = defaultdict(list)
+    subgrids = defaultdict(list)
+
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            cur_val = board[x][y]
+
+            # 0 automatically means invalid board
+            if cur_val == 0:
+                return False
+
+            try:
+                # Check the current row state
+                check_add_value(cur_val, rows[x])
+
+                # Check the current column state
+                check_add_value(cur_val, cols[y])
+
+                # Check the current subgrid state
+                check_add_value(cur_val, subgrids[(math.floor(x/3), math.floor(y/3))])
+            except:
+                return False
+
+    return True
+
+
+def check_add_value(val, values):
+    if val in values:
+        raise Exception()
+
+    values.append(val)
